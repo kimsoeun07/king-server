a1891198f59075a4b69647ede36a03a3c8ec8576
kimsoeun07 <110821356+kimsoeun07@users.noreply.github.com>
2025-06-16 20:38:48 +0900

    s

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-// const express = require('express');
-// const path = require('path');
-// const cors = require('cors');
 import express from 'express';
 import cors from 'cors';
-import path from 'path'; // 추가 필요
-const PORT = 5000; // 3000은 보통 프론트서버 기본 포트니까 다른 포트 추천
-
-;
+import path from 'path';
 
 const app = express();
+const PORT = 5000; // 3000은 보통 프론트서버 기본 포트니까 다른 포트 추천
 
-app.use(cors());
+app.use(cors()); // 일단 모든 origin 허용
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/main')));
-let seatData = []; // 메모리에 좌석 저장
 
-// 좌석 초기화 API
+let seatData = [];
+
 app.post("/api/seats/init", (req, res) => {
   seatData = req.body;
   console.log("초기 좌석 데이터:", seatData);
