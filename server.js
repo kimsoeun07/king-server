ee1ebdf2b5a1da5cfbcb0806f9595f1fd874591e
kimsoeun07 <110821356+kimsoeun07@users.noreply.github.com>
2025-06-16 20:27:51 +0900

    vercel.json 생성

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
 const app = express();
 const PORT = 3000;
-const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
@@ -10,9 +11,13 @@ app.use(express.static(path.join(__dirname, '/main')));
 
 let seatData = []; // 메모리에 좌석 저장
 
-// ✅ 좌석 초기화
-app.post("/seats/init", (req, res) => {
-  seatData = req.body; // 클라이언트가 보낸 좌석 배열 저장
+// 좌석 초기화 API
+app.post("/api/seats/init", (req, res) => {
+  seatData = req.body;
   console.log("초기 좌석 데이터:", seatData);
   res.json({ message: "좌석이 초기화되었습니다." });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
