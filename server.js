54cf86b66b2c1ed8f2bc5263223b078b9a229e23
kimsoeun07 <110821356+kimsoeun07@users.noreply.github.com>
2025-06-16 20:09:14 +0900

    post 주소 변경

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const PORT = 3000;
 const cors = require('cors');
 
 app.use(cors());
-
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '/main')));
 
 let seatData = []; // 메모리에 좌석 저장
 
 // ✅ 좌석 초기화
-app.post("/main", (req, res) => {
+app.post("/seats/init", (req, res) => {
   seatData = req.body; // 클라이언트가 보낸 좌석 배열 저장
   console.log("초기 좌석 데이터:", seatData);
   res.json({ message: "좌석이 초기화되었습니다." });
