851c591e28727db67e9dd21c96f2f7b3a0787b0d
kimsoeun07 <110821356+kimsoeun07@users.noreply.github.com>
2025-06-16 20:00:41 +0900

    .

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '/main')));
 
-app.post('/main', (req, res) => {
-    res.sendFile(path.join(__dirname, '/the_crown/src/app/main/page.tsx'));
-});
+let seatData = []; // 메모리에 좌석 저장
 
-app.listen(PORT, () => {
-    console.log(`START SERVER`);
+// ✅ 좌석 초기화
+app.post("/main", (req, res) => {
+  seatData = req.body; // 클라이언트가 보낸 좌석 배열 저장
+  console.log("초기 좌석 데이터:", seatData);
+  res.json({ message: "좌석이 초기화되었습니다." });
 });
