e9ad2ac747af35c54dd10625bf06d367b708490b
kimsoeun07 <110821356+kimsoeun07@users.noreply.github.com>
2025-06-16 20:50:30 +0900

    json파일 저장

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+
 
 const app = express();
 const PORT = 5000;
@@ -16,6 +18,20 @@ let seatData = [];
 app.post("/api/seats/init", (req, res) => {
   seatData = req.body;
   console.log("초기 좌석 데이터:", seatData);
+
+  console.log("초기 좌석 데이터:", seatData);
+
+  // JSON 파일로 저장
+  fs.writeFile('seats.json', JSON.stringify(seatData, null, 2), (err) => {
+    if (err) {
+      console.error("파일 저장 오류:", err);
+      return res.status(500).json({ message: "파일 저장 중 오류가 발생했습니다." });
+    }
+
+  })
+
+
+  
   res.json({ message: "좌석이 초기화되었습니다." });
 });
 
